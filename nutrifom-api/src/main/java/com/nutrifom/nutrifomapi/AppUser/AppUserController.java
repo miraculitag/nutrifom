5e3b6e29c832ee26432112a863172161eba32b29
Maid Sabanovic <maid768@gmail.com>
2023-10-16 20:24:02 +0200

    Added put endpoints

--- nutrifom-api/src/main/java/com/nutrifom/nutrifomapi/AppUser/AppUserController.java
+++ nutrifom-api/src/main/java/com/nutrifom/nutrifomapi/AppUser/AppUserController.java
@@ -18,36 +18,37 @@ public class AppUserController {
         this.appUserService = appUserService;
     }
 
-    @GetMapping("/get/all")
-    public List<AppUser> getUsers() {
-        return appUserService.getAppUsers();
+    @GetMapping("/get/{appUserEmail}")
+    public Optional<AppUser> getUserByEmail(@PathVariable("appUserEmail") String appUserEmail) {
+        return appUserService.getAppUserByEmail(appUserEmail);
     }
 
-    @GetMapping("/get/{appUserID}")
-    public Optional<AppUser> getUserByID(@PathVariable("appUserID") Integer appUserID) {
-        return appUserService.getAppUserByID(appUserID);
+    @PutMapping("/put/goal/{goal}")
+    public ResponseEntity<String> updateGoal(@PathVariable String goal, @RequestParam String email) {
+        return appUserService.updateAppUserGoal(email, goal);
     }
 
-    @PostMapping("/post")
-    public AppUser addUser(@RequestBody AppUser appUser) {
-        return appUserService.addAppUser(appUser);
+    @PutMapping("/put/weight/{weight}")
+    public ResponseEntity<String> updateWeight(@PathVariable int weight, @RequestParam String email) {
+        return appUserService.updateAppUserWeight(email, weight);
     }
 
-    @PutMapping(path = "/put/{appUserID}")
-    public ResponseEntity<AppUser> updateAppUser(@PathVariable("appUserID") Integer appUserID, @RequestBody AppUser updatedAppUser) {
+    @PutMapping("/put/pal/{pal}")
+    public ResponseEntity<String> updatePal(@PathVariable String pal, @RequestParam String email) {
+        return appUserService.updateAppUserPal(email, pal);
+    }
+
+    @PutMapping("/put/imageBlobUrl/{imageBlobUrl}")
+    public ResponseEntity<String> updateImageBlobUrl(@PathVariable String imageBlobUrl, @RequestParam String email) {
+        return appUserService.updateAppUserImageBlobUrl(email, imageBlobUrl);
+    }
+
+
+    @DeleteMapping(path = "/del/{appUserEmail}")
+    public ResponseEntity<String> deleteAppUser(@PathVariable("appUserEmail") String email) {
         try {
-            AppUser appUser = appUserService.updateAppUser(appUserID, updatedAppUser);
-            return new ResponseEntity<>(appUser, HttpStatus.OK);
-        } catch (IllegalStateException e) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @DeleteMapping(path = "/del/{appUserID}")
-    public ResponseEntity<String> deleteAppUser(@PathVariable("appUserID") Integer appUserID) {
-        try {
-            appUserService.deleteAppUser(appUserID);
-            return new ResponseEntity<>("User with ID " + appUserID + " deleted successfully.", HttpStatus.OK);
+            appUserService.deleteAppUserByEmail(email);
+            return new ResponseEntity<>("User with email" + email + " deleted successfully.", HttpStatus.OK);
         } catch (IllegalStateException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
--- nutrifom-api/src/main/java/com/nutrifom/nutrifomapi/AppUser/AppUserService.java
+++ nutrifom-api/src/main/java/com/nutrifom/nutrifomapi/AppUser/AppUserService.java
@@ -1,6 +1,8 @@
 package com.nutrifom.nutrifomapi.AppUser;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,23 +16,14 @@ public class AppUserService {
         this.appUserRepository = appUserRepository;
     }
 
-    public List<AppUser> getAppUsers() {
-        return appUserRepository.findAll();
-    }
-
-    public Optional<AppUser> getAppUserByID(Integer id) {
-        return appUserRepository.findById(id);
-    }
-
-    public AppUser addAppUser(AppUser appUser) {
-        return appUserRepository.save(appUser);
+    public Optional<AppUser> getAppUserByEmail(String email) {
+        return appUserRepository.findByEmail(email);
     }
 
     public AppUser updateAppUser(Integer id, AppUser updatedAppUser) {
         AppUser existingAppUser = appUserRepository.findById(id)
                 .orElseThrow(() -> new IllegalStateException("User with ID " + id + " doesn't exist"));
 
-        existingAppUser.setName(updatedAppUser.getName());
         existingAppUser.setDob(updatedAppUser.getDob());
         existingAppUser.setWeight(updatedAppUser.getWeight());
         existingAppUser.setGoal(updatedAppUser.getGoal());
@@ -43,13 +36,69 @@ public class AppUserService {
         return appUserRepository.save(existingAppUser);
     }
 
+    public ResponseEntity<String> updateAppUserGoal(String email, String updatedGoal) {
+        Optional<AppUser> existingAppUserOptional = appUserRepository.findByEmail(email);
 
-
-    public void deleteAppUser(Integer appUserID) {
-        boolean exists = appUserRepository.existsById(appUserID);
-        if (!exists) {
-            throw new IllegalStateException("User with ID " + appUserID + " doesn't exist");
+        if (!existingAppUserOptional.isPresent()) {
+            return new ResponseEntity<>("User with email " + email + " doesn't exist", HttpStatus.NOT_FOUND);
         }
-        appUserRepository.deleteById(appUserID);
+
+        AppUser existingAppUser = existingAppUserOptional.get();
+        existingAppUser.setGoal(updatedGoal);
+        appUserRepository.save(existingAppUser);
+
+        return new ResponseEntity<>("Updated goal: " + updatedGoal + " for " + email, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> updateAppUserWeight(String email, int updatedWeight) {
+        Optional<AppUser> existingAppUserOptional = appUserRepository.findByEmail(email);
+
+        if (!existingAppUserOptional.isPresent()) {
+            return new ResponseEntity<>("User with email " + email + " doesn't exist", HttpStatus.NOT_FOUND);
+        }
+
+        AppUser existingAppUser = existingAppUserOptional.get();
+        existingAppUser.setWeight(updatedWeight);
+        appUserRepository.save(existingAppUser);
+
+        return new ResponseEntity<>("Updated Weight: " + updatedWeight + " for " + email, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> updateAppUserPal(String email, String updatedPal) {
+        Optional<AppUser> existingAppUserOptional = appUserRepository.findByEmail(email);
+
+        if (!existingAppUserOptional.isPresent()) {
+            return new ResponseEntity<>("User with email " + email + " doesn't exist", HttpStatus.NOT_FOUND);
+        }
+
+        AppUser existingAppUser = existingAppUserOptional.get();
+        existingAppUser.setPal(updatedPal);
+        appUserRepository.save(existingAppUser);
+
+        return new ResponseEntity<>("Updated PAL: " + updatedPal + " for " + email, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> updateAppUserImageBlobUrl(String email, String updatedImageBlobUrl) {
+        Optional<AppUser> existingAppUserOptional = appUserRepository.findByEmail(email);
+
+        if (!existingAppUserOptional.isPresent()) {
+            return new ResponseEntity<>("User with email " + email + " doesn't exist", HttpStatus.NOT_FOUND);
+        }
+
+        AppUser existingAppUser = existingAppUserOptional.get();
+        existingAppUser.setImageBlobUrl(updatedImageBlobUrl);
+        appUserRepository.save(existingAppUser);
+
+        return new ResponseEntity<>("Updated Image: " + updatedImageBlobUrl + " for " + email, HttpStatus.OK);
+    }
+
+
+
+    public void deleteAppUserByEmail(String email) {
+        Optional<AppUser> foundUser = appUserRepository.findByEmail(email);
+        if (!foundUser.isPresent()) {
+            throw new IllegalStateException("User with email " + email + " doesn't exist");
+        }
+        appUserRepository.deleteById(foundUser.get().getId());
     }
 }
