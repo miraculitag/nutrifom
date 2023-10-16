e0da2ccfe288180e0d4937633358eaa3faaa637a
mira.falk <mira.falk@zeb.de>
2023-10-16 19:00:47 +0200

    added theme template and test user profile

--- nutrifom-ui/src/App.tsx
+++ nutrifom-ui/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import nutrifomTheme from "./theme/nutrifomTheme";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { User } from "./components/User";
@@ -7,21 +8,24 @@ import { Weight } from "./components/Weight";
 import { Calc } from "./components/Calc";
 import { Recipes } from "./components/Recipes";
 import { Header } from "./components/Header";
+import { ThemeProvider } from "@mui/material";
 
 export default function App() {
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/foodlog" element={<FoodLog />} />
-          <Route path="/weight" element={<Weight />} />
-          <Route path="/calc" element={<Calc />} />
-          <Route path="/recipes" element={<Recipes />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={nutrifomTheme}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/foodlog" element={<FoodLog />} />
+            <Route path="/weight" element={<Weight />} />
+            <Route path="/calc" element={<Calc />} />
+            <Route path="/recipes" element={<Recipes />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
