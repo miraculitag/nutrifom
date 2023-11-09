cd19dab633e7acc94dd61dfcd7c1448940ecdada
Maid Sabanovic <maid768@gmail.com>
2023-11-09 22:28:00 +0100

    Added rating and uses functionality to recipees

--- nutrifom-api/src/main/java/com/nutrifom/nutrifomapi/Recipe/RecipeService.java
+++ nutrifom-api/src/main/java/com/nutrifom/nutrifomapi/Recipe/RecipeService.java
@@ -1,16 +1,23 @@
 package com.nutrifom.nutrifomapi.Recipe;
 
+import com.nutrifom.nutrifomapi.AppUser.AppUser;
+import com.nutrifom.nutrifomapi.AppUser.AppUserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RecipeService {
     @Autowired
     private RecipeRepository recipeRepository;
+
+    @Autowired
+    private AppUserRepository appUserRepository;
 
     public List<Recipe> getRecipesByTag(String tag) {
         Specification<Recipe> spec = (root, query, criteriaBuilder) ->
@@ -20,5 +27,49 @@ public class RecipeService {
 
         return recipeRepository.findAll(spec, sort);
     }
+
+    public Recipe rateRecipe(Integer recipeId, Integer userId, Double score) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new EntityNotFoundException("Rezept nicht gefunden"));
+        AppUser user = appUserRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("Benutzer nicht gefunden"));
+
+        Optional<Rating> existingRating = recipe.getRatings().stream()
+                .filter(r -> r.getAppUser().getId() == userId)
+                .findFirst();
+
+        if (existingRating.isPresent()) {
+            existingRating.get().setScore(score);
+        } else {
+            Rating newRating = new Rating();
+            newRating.setAppUser(user);
+            newRating.setScore(score);
+            newRating.setRecipe(recipe);
+            recipe.getRatings().add(newRating);
+        }
+
+        // Berechne das durchschnittliche Rating und speichere es im Recipe-Objekt
+        double averageRating = recipe.getRatings().stream()
+                .mapToDouble(Rating::getScore)
+                .average()
+                .orElse(0.0);
+
+        recipe.setAverageRating(averageRating); // Setze das durchschnittliche Rating
+
+        return recipeRepository.save(recipe); // Speichere das aktualisierte Rezept
+    }
+
+    public Recipe incrementRecipeUses(Integer recipeId) {
+        // Rezept nach ID suchen
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new EntityNotFoundException("Rezept nicht gefunden"));
+
+        // Click-Wert um eins erhöhen
+        recipe.setUses(recipe.getUses() + 1);
+
+        // Rezept mit aktualisiertem Click-Wert speichern
+        return recipeRepository.save(recipe);
+    }
+
 
 }
