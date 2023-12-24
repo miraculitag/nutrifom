e782e3ded065413e019a764e79eb3980dd1bfc31
mira.falk <mira.falk@zeb.de>
2023-12-24 14:01:09 +0100

    improved footer position

--- nutrifom-ui/src/components/layout/Footer.tsx
+++ nutrifom-ui/src/components/layout/Footer.tsx
@@ -8,29 +8,27 @@ import CopyrightIcon from "@mui/icons-material/Copyright";
 
 export const Footer = (testParams: any) => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar sx={{ position: "static", marginTop: "2%" }}>
-        <Toolbar
-          sx={{
-            backgroundColor: "gray",
-            color: "white",
+    <AppBar sx={{ position: "static", marginTop: "2%" }}>
+      <Toolbar
+        sx={{
+          backgroundColor: "gray",
+          color: "white",
+        }}
+      >
+        <Typography>nutrifom GmbH</Typography>
+        <CopyrightIcon
+          sx={{ margin: "10px", fontSize: "medium" }}
+        ></CopyrightIcon>
+        <Typography sx={{ flexGrow: 1 }}>2024</Typography>
+        <Button
+          color="inherit"
+          onClick={() => {
+            window.open("mailto:email@example.com");
           }}
         >
-          <Typography>nutrifom GmbH</Typography>
-          <CopyrightIcon
-            sx={{ margin: "10px", fontSize: "medium" }}
-          ></CopyrightIcon>
-          <Typography sx={{ flexGrow: 1 }}>2024</Typography>
-          <Button
-            color="inherit"
-            onClick={() => {
-              window.open("mailto:email@example.com");
-            }}
-          >
-            Kontakt
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+          Kontakt
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
