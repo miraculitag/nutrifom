4aff0079a127a96f9f2fae90ebfebef00c03cf5c
CreativeMind42 <jennifer.witte@fom-net.de>
2023-12-28 19:28:10 +0100

    Implement new Recipie and FoodSearch with React.useEffect

--- nutrifom-ui/src/components/partials/FoodSearch.tsx
+++ nutrifom-ui/src/components/partials/FoodSearch.tsx
@@ -7,14 +7,6 @@ import dayjs from "dayjs";
 import useAuthHeader from "react-auth-kit/dist/hooks/useAuthHeader";
 import { FoodEntry } from "../../types";
 
-function sleep(duration: number): Promise<void> {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, duration);
-  });
-}
-
 export default function FoodSearch() {
   const auth = useAuthHeader();
   const currentDate = dayjs().format("YYYY-MM-DD");
@@ -24,26 +16,35 @@ export default function FoodSearch() {
   const [foodAmountHasError, setFoodAmountHasError] = React.useState(false);
   const [apiData, setApiData] = React.useState<FoodEntry[]>([]);
   const [searchTextFood, setSearchTextFood] = React.useState("");
-  const [selectedFood, setSelectedFood] = React.useState<FoodEntry | null>(
-    null
-  );
-  const [loading, setLoading] = React.useState(false);
+  const [selectedFood, setSelectedFood] = React.useState<
+    FoodEntry | undefined
+  >();
+
+
+  React.useEffect(() => {
+    if (searchTextFood) {
+      if (!selectedFood) {
+        try {
+          searchOFF(searchTextFood, auth()).then((response) => {
+            setApiData(response.data);
+            console.log(response.data);
+          });
+        } catch (error) {
+          console.log("Fehler beim Abrufen der offSearch:", error);
+        } 
+      }
+    }
+  }, [searchTextFood]);
 
   const handleChangeSearchFoodTextAmount = () => {
     setFoodAmountHasError(false);
     setFoodSearchHasError(false);
 
     const dateString: string = currentDate;
-    console.log(
-      "Sende Daten ans Backend",
-      selectedFood?.productCode || "123",
-      dateString,
-      currentFoodAmount
-    );
 
     addProductToNutrilog(
       {
-        productCode: selectedFood?.productCode || "123",
+        productCode: selectedFood?.productCode || "",
         entryDate: dateString,
         productQuantity: currentFoodAmount,
       },
@@ -53,73 +54,43 @@ export default function FoodSearch() {
         console.log("Error 403 while putting weight:", auth());
       }
     });
-    console.log("Daten versendet");
-  };
-
-  const handleSearchTextChange = async (
-    event: ChangeEvent<HTMLInputElement>
-  ) => {
-    setSearchTextFood(event.target.value);
-
-    if (searchTextFood !== "") {
-      try {
-        setLoading(true);
-        await sleep(1000); // Simulate asynchronous behavior
-
-        const response = await searchOFF(searchTextFood, auth());
-        setApiData(response.data);
-      } catch (error) {
-        console.log("Fehler beim Abrufen der offSearch:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
   };
 
   return (
     <>
       <Box sx={{ marginBottom: "5%" }}>
-        <Autocomplete
-          id="free-solo-2-demo"
-          disableClearable
-          options={apiData}
-          getOptionLabel={(apiData) => apiData.productName}
-          isOptionEqualToValue={(option, value) =>
-            option.productName === value.productName
-          }
-          onChange={(event, value) => setSelectedFood(value)}
-          loading={loading}
-          renderInput={(params) => (
-            <TextField
-              label="Suche hier nach einem Lebensmittel..."
-              variant="standard"
-              onChange={handleSearchTextChange}
-              value={searchTextFood}
-              required={false}
-              {...params}
-              InputProps={{
-                ...params.InputProps,
-                endAdornment: (
-                  <React.Fragment>
-                    {loading ? (
-                      <CircularProgress color="inherit" size={20} />
-                    ) : null}
-                    {params.InputProps.endAdornment}
-                  </React.Fragment>
-                ),
-              }}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              error={FoodSearchHasError}
-              helperText={
-                FoodSearchHasError
-                  ? "Du hast kein Lebensmittel eingegeben."
-                  : ""
-              }
-            />
-          )}
-        />
+          <Autocomplete
+            onChange={(event: any, newValue: FoodEntry | null) => {
+              setSelectedFood(newValue || undefined);
+            }}
+            inputValue={searchTextFood}
+            onInputChange={(event, newInputValue) => {
+              setSearchTextFood(newInputValue);
+            }}
+            id="controllable-states-demo"
+            options={apiData}
+            getOptionLabel={(apiData) => apiData?.productName || ""}
+            isOptionEqualToValue={(option, value) =>
+              option?.productCode === value?.productCode
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Suche hier nach einem Lebensmittel..."
+                variant="standard"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                error={FoodSearchHasError}
+                required={true}
+                helperText={
+                  FoodSearchHasError
+                    ? "Du hast kein Lebensmittel eingegeben."
+                    : ""
+                }
+              />
+            )}
+          />
       </Box>
       <Box sx={{ marginBottom: "5%" }}>
         <FloatInputField
@@ -130,7 +101,7 @@ export default function FoodSearch() {
           hasError={foodAmountHasError}
           errorText="Die Menge kann nicht negativ oder 0 sein."
           width="100%"
-          required={false}
+          required={true}
         />
       </Box>
       <BasicButton
