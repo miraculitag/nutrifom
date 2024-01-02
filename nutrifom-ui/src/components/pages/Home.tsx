b421945570c071089e3dc4646ce3aeda5c11e988
mira.falk <mira.falk@zeb.de>
2024-01-02 19:05:10 +0100

    corrected spelling

--- nutrifom-ui/src/components/pages/Home.tsx
+++ nutrifom-ui/src/components/pages/Home.tsx
@@ -11,7 +11,7 @@ export const Home = () => {
     'Die nutrifom GmbH stellt die Bedürfnisse ihrer Kunden in den Vordergrund. Seit 2023 schafft die nutrifom GmbH neue Lösungen zur Unterstützung bei einer gesunden Ernährung. "nutrifom" die Webanwendung stellt hierbei das Erreichen deiner individuellen Ziele in den Fokus. Hier findest du eine Übersicht und eine Vorstellung der enthaltenen Features und außerdem einige hilfreiche Tipps für eine gesunde Ernährung. Falls du Hilfe benötigen solltest, kontaktiere uns gerne über das Kontaktfeld. Dieses findest du am unteren Seitenrand.';
 
   const aboutNutrifomNutrilog =
-    "Das Nutriprotokoll dient der Protokollierung deiner Ernährung. Sowohl Speisen als auch Getränke kannst du hier eintragen. Auswählen kannst du hierbei aus einer großen Datenbank an Nahrungsmitteln. Auch Rezepte, welche du in unserem Rezeptebereich entdeckt und nachgekocht hast, kannst du hier portionsweise eintragen. Deine tägliche Kalorienbilanz wird übersichtlich dargestellt, wobei du auch die Zusammensetzung deiner Ernährung einsehen kannst.";
+    "Das Nutriprotokoll dient der Protokollierung deiner Ernährung. Sowohl Speisen als auch Getränke kannst du hier eintragen. Auswählen kannst du hierbei aus einer großen Datenbank an Nahrungsmitteln. Auch Rezepte, welche du in unserem Rezeptbereich entdeckt und nachgekocht hast, kannst du hier portionsweise eintragen. Deine tägliche Kalorienbilanz wird übersichtlich dargestellt, wobei du auch die Zusammensetzung deiner Ernährung einsehen kannst.";
 
   const aboutNutrifomWeight =
     "Der Gewichtsbereich dient der Protokollierung deines Gewichts. Täglich kannst du hier dein Gewicht eintragen und deinen Gewichtsverlauf beobachten. Auch zur Berechnung deines individuellen Energiebedarfs ist dein Gewichtsverlauf hilfreich.";
@@ -20,7 +20,7 @@ export const Home = () => {
     "Der Bedarfsrechner dient zur Berechnung deines Energiebedarfs. Dein Energiebedarf ist sehr individuell und hängt neben Parametern wie Alter, Größe, Geschlecht oder Gewicht auch von individuellen Gegebenheiten ab. Beispiele hierfür sind die Körpergewichtszusammensetzung, der Hormonhaushalt oder Stress. Daher verwendet der Bedarfsrechner zu Beginn eine standardisierte Formel, später, wenn ausreichend Daten vorhanden sind, jedoch dein individuelles Nutri- und Gewichtsprotokoll. Darüber hinaus hast du die Möglichkeit, dein individuelles Ziel bezüglich deines Körpergewichts anzugeben, welches dann bei der Berechnung berücksichtigt wird.";
 
   const aboutNutrifomRecipes =
-    'Im Rezeptebereich steht dir eine Auswahl an gesunden Rezepten zur Verfügung. Die Nährwerte der Gerichte sind übersichtlich dargestellt und können dir dabei helfen, dein individuelles Kalorienziel zu erreichen. Rezepte mit einer höheren Kaloriendichte werden hierbei unter der Kategorie "Aufbauen" gelistet, während Rezepte mit niedrigerer Kaloriendichte der Kategorie "Definieren" zugeordnet sind. Beachte: Auch die Rezepte mit höherer Kaloriendichte sind gesund!';
+    'Im Rezeptbereich steht dir eine Auswahl an gesunden Rezepten zur Verfügung. Die Nährwerte der Gerichte sind übersichtlich dargestellt und können dir dabei helfen, dein individuelles Kalorienziel zu erreichen. Rezepte mit einer höheren Kaloriendichte werden hierbei unter der Kategorie "Aufbauen" gelistet, während Rezepte mit niedrigerer Kaloriendichte der Kategorie "Definieren" zugeordnet sind. Beachte: Auch die Rezepte mit höherer Kaloriendichte sind gesund!';
 
   const infoTextMicros =
     "Mineralstoffe und Vitamine erfüllen wichtige Funktionen innerhalb des Körpers. Besonders Obst und Gemüse, aber auch Vollkornprodukte, sind zur Aufnahme von ausreichend Mikronährstoffen geeignet. Empfohlen werden täglich mindestens fünf Portionen rohes oder schonend zubereitetes Gemüse (min. 400g) und Obst (min. 250g). Als besonders mikronährstoffreich gilt hierbei grünes Gemüse.";
@@ -29,10 +29,10 @@ export const Home = () => {
     "Proteine sättigen besonders gut. Außerdem sind sie essentiell für Muskelaufbau und -erhalt. Die Quelle der Proteine entscheidet über die Qualität und die Verwertbarkeit der Proteine. Tierische Proteine haben meist eine höhere biologische Wertigkeit als pflanzliche Proteine. Auch die Kombination bestimmter Nahrungsmittel kann dir helfen, die biologische Wertigkeit deiner Mahlzeit zu erhöhen. Das geht beispielsweise mit Kartoffeln + Ei, Soja + Reis oder Mais + Bohnen.";
 
   const infoTextFat =
-    "Achte bei dem Verzehr von Fetten auch auf die Quellen. Besonders Nüsse und pflanzliche Öle liefern gesunde ungesättigte Fettsäuren, welche positive Effekte auf den Körper haben. Beispiele hierfür sind die Hirnfuktion oder das Immunsystem.";
+    "Achte bei dem Verzehr von Fetten auch auf die Quellen. Besonders Nüsse und pflanzliche Öle liefern gesunde ungesättigte Fettsäuren, welche positive Effekte auf den Körper haben. Beispiele hierfür sind die Hirnfunktion oder das Immunsystem.";
 
   const infoTextCarbs =
-    "Besonders der Verzehr komplexer Kohlenhydraten ist vorteilhaft. Sie dienen als Träger von Mineralstoffen, Vitaminen und Ballaststoffen. Versuche deine Kohlenhydrate aus Vollkornprodukten anstelle von Weißmehlprodukten oder mit Zucker gesüßten Waren zu gewinnen.";
+    "Besonders der Verzehr komplexer Kohlenhydrate ist vorteilhaft. Sie dienen als Träger von Mineralstoffen, Vitaminen und Ballaststoffen. Versuche, deine Kohlenhydrate aus Vollkornprodukten anstelle von Weißmehlprodukten oder mit Zucker gesüßten Waren zu gewinnen.";
 
   const aboutFeatureTexts = [
     {
